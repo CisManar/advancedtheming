4483cc48a8b07c572082bfacdb4afdb265a2f85b
cisManar <cis.manarsarah@gmail.com>
2018-04-21 10:29:17 +0300

    add new blocks

--- sites/all/themes/smile/templates/page.tpl.php
+++ sites/all/themes/smile/templates/page.tpl.php
@@ -105,7 +105,9 @@
 	</header>
 
 	<section id="main-slider" class="no-margin">
-		<?php print render($page['main-slider']); ?>
+		<div class="carousel slide">      
+				<?php print render($page['main-slider']); ?>
+		</div>
 	</section>
 
 	<div class="feature">
@@ -121,6 +123,46 @@
 			<?php print render($page['about']); ?>
 		</div>
 	</div>
+
+	<div class="lates">
+		<div class="container">
+			<div class="text-center">
+				<h2>Lates News</h2>
+			</div>
+			<?php print render($page['latest']); ?>
+		</div>
+	</div>
+
+	<footer>
+		<div class="footer">
+			<div class="container">
+				<div class="social-icon">
+					<div class="col-md-4">
+						<ul class="social-network">
+							<li><a href="#" class="fb tool-tip" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+							<li><a href="#" class="twitter tool-tip" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+							<li><a href="#" class="gplus tool-tip" title="Google Plus"><i class="fa fa-google-plus"></i></a></li>
+							<li><a href="#" class="linkedin tool-tip" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+							<li><a href="#" class="ytube tool-tip" title="You Tube"><i class="fa fa-youtube-play"></i></a></li>
+						</ul>	
+					</div>
+				</div>
+				
+				<div class="col-md-4 col-md-offset-4">
+					<div class="copyright">
+						&copy; 2018 <a target="_blank" href="http://www.facebook.com/cisManar" >Manar Sarah</a>. All Rights Reserved.
+					</div>
+
+				</div>						
+			</div>
+			
+			<div class="pull-right">
+				<a href="#" class="scrollup"><i class="fa fa-angle-up fa-3x"></i></a>
+			</div>		
+		</div>
+	</footer>
+
+
 
 </div></div>
 
